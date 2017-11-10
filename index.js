174ac1fd7f17bc12d52c5159c18afe5b9b65e4e9
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-11-10 10:19:29 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+class MenuDemo extends Component {
+
+  render(){
+	  
+    return (
+      <div>
+	      {this.props.text}
+      </div>
+    );
+	  
+  }
+}
+
+export default MenuDemo;
